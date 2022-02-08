58d9cfb8385defcde09a9bdf685b8958f3eb35f2
Ali Haji Aghakhani <aliaghakhani911@gmail.com>
2022-02-08 23:38:01 +0330

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -6,18 +6,44 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/dashboard',
+    //name: 'Dashboard',
+    component: () => import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue'),
+    children: [
+
+        {
+    path: '',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
+    path: 'about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+      
+    ]
+
+  
+  },
+  {
+    path: '/auth',
+    //name: 'Dashboard',
+    component: () => import(/* webpackChunkName: "auth" */ '../views/Auth.vue'),
+    children: [
+            
+          {
+             path: 'login',
+             name: 'Login',
+             component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+          }
+      
+    ]
+
+  
+  },
+
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    elevation="2"
+
+ 
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          صفحه ورود
+        </div>
+
+      </v-list-item-content>
+
+    
+    </v-list-item>
+           <v-col
+          cols="12"
+          >
+          <v-text-field
+            label="نام کاربری یا ایمیل"
+            outlined
+            dense
+          ></v-text-field>
+          <v-text-field
+            outlined
+            :type ="showPassword ? 'text' : 'password' "
+            label="رمز عبور"
+            :prepend-inner-icon="showPassword?'mdi-eye' : 'mdi-eye-off'"
+            @click:prepend-inner="showPassword = !showPassword"
+          ></v-text-field>
+        </v-col>
+
+    <v-card-actions>
+       <v-btn
+      depressed
+      color="primary"
+    >
+      ورود
+    </v-btn>
+       <v-btn
+      depressed
+      color="primary"
+    >
+      فراموشی رمز عبور
+    </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+
+  data : () =>({
+    showPassword : false ,
+  })
+
+// setup(props) {
+
+
+// const showPassword = ref(false)
+
+// return{
+//   showPassword
+// }
+  
+// }
+
+}
+</script>
+
+<style>
+.v-text-field__slot{
+  direction: ltr!important;
+}
+</style>
