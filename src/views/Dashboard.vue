9ef8ac2c99c885327d4dce1dde413ec83cb4573b
Ali Haji Aghakhani <aliaghakhani911@gmail.com>
2022-02-09 02:18:26 +0330

    building home-page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,52 +1,121 @@
 <template>
     <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+      app
+    >
+       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>پنل ادمین</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
       </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
+    <v-navigation-drawer app right v-model="drawer" >
+      <template #prepend >
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="https://avatars.githubusercontent.com/u/19605594?v=4" />
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>
+              علی آقاخانی
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              مدیر سایت
+            </v-list-item-subtitle>
+          </v-list-item-content>
+
+        </v-list-item>
+      </template>
+      <v-list nav dense >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>داشبورد</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>اطلاعات کاربری</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+
+      <template #append >
+        <div class=" mx-4 ">
+        <v-btn block class="mb-2" color="grey darken-4" dark>
+          خروج
+        </v-btn>
+        </div>
+       
+      </template>
+
+    </v-navigation-drawer>
 
     <v-main>
       <router-view/>
     </v-main>
+      <v-footer padless>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>powered by Javascript</strong>
+    </v-col>
+  </v-footer>
   </v-app>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    data: () => ({
+      drawer: true,
+      group: null,
+    }),
+  }
 </script>
 
 <style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,68 @@
 <template>
- 
+   <v-container>
+     <v-row>
+       <v-col cols="12" sm="6" md="3">
+         <v-card class="green">
+           <v-card-text class="d-flex white--text">
+             <span>
+                  آیتم
+             </span>
+             <v-spacer>
+
+             </v-spacer>
+             <v-icon class="green--text text--lighten-5">
+               mdi-post
+             </v-icon>
+           </v-card-text>
+         </v-card>
+       </v-col>
+       <v-col cols="12" sm="6" md="3">
+         <v-card class="indigo">
+           <v-card-text class="d-flex white--text">
+             <span>
+                  آیتم
+             </span>
+             <v-spacer>
+
+             </v-spacer>
+             <v-icon class="green--text text--lighten-5">
+               mdi-post
+             </v-icon>
+           </v-card-text>
+         </v-card>
+       </v-col>
+       <v-col cols="12" sm="6" md="3">
+         <v-card class="blue">
+           <v-card-text class="d-flex white--text">
+             <span>
+                  آیتم
+             </span>
+             <v-spacer>
+
+             </v-spacer>
+             <v-icon class="green--text text--lighten-5">
+               mdi-post
+             </v-icon>
+           </v-card-text>
+         </v-card>
+       </v-col>
+       <v-col cols="12" sm="6" md="3">
+         <v-card class="orange">
+           <v-card-text class="d-flex white--text">
+             <span>
+                  آیتم
+             </span>
+             <v-spacer>
+
+             </v-spacer>
+             <v-icon class="green--text text--lighten-5">
+               mdi-post
+             </v-icon>
+           </v-card-text>
+         </v-card>
+       </v-col>
+     </v-row>
+   </v-container>
 </template>
 
 <script>
@@ -9,7 +72,7 @@
     name: 'Home',
 
     components: {
-      HelloWorld,
+
     },
   }
 </script>
