99476c3d1247625961319b20611b6475e23fc9b2
Ali Haji Aghakhani <aliaghakhani911@gmail.com>
2022-02-14 01:06:52 +0330

    user page

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,133 +1,153 @@
 <template>
-  <v-card>
+<v-card>
     <v-card-title>
-      کاربران
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="جستجو"
-        hide-details
-      ></v-text-field>
+        کاربران
+        <v-spacer></v-spacer>
+        <v-text-field v-model="search" append-icon="mdi-magnify" label="جستجو" hide-details></v-text-field>
     </v-card-title>
 
     <v-card class="mx-5">
-      
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :search="search"
-    ></v-data-table>
-     
+
+        <v-data-table :headers="headers" :items="usersInfoList" :search="search" class="elevation-1">
+            <template slot:top>
+                <v-toolbar>
+                    <v-dialog v-model="dialogDelete" max-width="500px">
+                        <v-card>
+                            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                                <v-spacer></v-spacer>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                </v-toolbar>
+            </template>
+            <template v-slot:item.actions="{ item }">
+                <v-icon small class="mr-2" @click="editItem(item)">
+                    mdi-pencil
+                </v-icon>
+                <v-icon small @click="deleteItem(item)">
+                    mdi-delete
+                </v-icon>
+            </template>
+        </v-data-table>
+
     </v-card>
 
-
-  </v-card>
+</v-card>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        search: '',
-        headers: [
-          {
-            text: 'نام',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'نام کاربری', value: 'calories' },
-          { text: 'ایمیل', value: 'fat' },
-          { text: 'شماره تماس', value: 'carbs' },
-          { text: 'پست ها', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' },
-        ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
-        ],
-      }
+import axios from '../plugins/axios'
+export default {
+    data() {
+        return {
+            dialog: false,
+            dialogDelete: false,
+            search: '',
+            headers: [{
+                    text: 'نام',
+                    align: 'start',
+                    sortable: false,
+                    value: 'name',
+                },
+                {
+                    text: 'نام کاربری',
+                    value: 'slug'
+                },
+                {
+                    text: 'ایمیل',
+                    value: 'fat'
+                },
+                {
+                    text: 'شماره تماس',
+                    value: 'carbs'
+                },
+                {
+                    text: 'پست ها',
+                    value: 'protein'
+                },
+                {
+                    text: 'Iron (%)',
+                    value: 'iron'
+                },
+            ],
+            usersInfoList: [],
+        }
     },
-  }
+    methods: {
+
+        getUsers: async function () {
+
+            try {
+
+                const {
+                    data
+                } = await axios.get('/wp-json/wp/v2/users', {
+                    headers: {
+                        'Authorization': `Bearer ${localStorage.getItem('userToken')}`
+                    }
+                })
+                this.usersInfoList.push(...data)
+                console.log(data, 'is user informations');
+            } catch (e) {
+                console.log('we have some errors');
+            }
+
+        },
+        editItem(item) {
+            this.editedIndex = this.desserts.indexOf(item)
+            this.editedItem = Object.assign({}, item)
+            this.dialog = true
+        },
+
+        deleteItem(item) {
+            this.editedIndex = this.desserts.indexOf(item)
+            this.editedItem = Object.assign({}, item)
+            this.dialogDelete = true
+        },
+
+        deleteItemConfirm() {
+            this.desserts.splice(this.editedIndex, 1)
+            this.closeDelete()
+        },
+        deleteItemConfirm() {
+            this.desserts.splice(this.editedIndex, 1)
+            this.closeDelete()
+        },
+
+        close() {
+            this.dialog = false
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            })
+        },
+
+        closeDelete() {
+            this.dialogDelete = false
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            })
+        },
+
+        save() {
+            if (this.editedIndex > -1) {
+                Object.assign(this.desserts[this.editedIndex], this.editedItem)
+            } else {
+                this.desserts.push(this.editedItem)
+            }
+            this.close()
+        },
+
+    },
+    created() {
+        this.getUsers();
+    },
+}
 </script>
 
 <style>
