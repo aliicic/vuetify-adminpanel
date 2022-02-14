d896a2a78ef47f6ca04779f4b2dca1db8c007cf0
Ali Haji Aghakhani <aliaghakhani911@gmail.com>
2022-02-14 16:13:25 +0330

    replace user datatable with better table

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,41 +1,72 @@
 <template>
-<v-card class="ma-10 pa-5">
-    <v-card-title>
-        کاربران
-        <v-spacer></v-spacer>
-        <v-text-field v-model="search" append-icon="mdi-magnify" label="جستجو" hide-details></v-text-field>
-    </v-card-title>
+<v-data-table :headers="headers" :items="usersInfoList" sort-by="calories" class="elevation-1" :loading="loading" loading-text="لطفا منتظر بمانید">
+    <template v-slot:top>
+        <v-toolbar flat>
+            <v-toolbar-title>کاربران</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+                        اضافه کردن کاربر جدید
+                    </v-btn>
+                </template>
+                <v-card>
+                    <v-card-title>
+                        <span class="text-h5">عنوان</span>
+                    </v-card-title>
 
-    <v-card class="mx-5">
+                    <v-card-text>
+                        <v-container>
+                            <v-row>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-text-field v-model="editedItem.slug" label="Calories"></v-text-field>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-card-text>
 
-        <v-data-table :headers="headers" :items="usersInfoList" :search="search" class="elevation-1" :loading="loading" loading-text="لطفا چند لحظه صبر کنید">
-            <template v-slot:item.actions="{ item }">
-
-                <v-dialog v-model="dialogDelete" max-width="500px">
-                    <v-card>
-                        <v-card-title class="text-h6">آیا واقعا و جدی جدی میخوای این کاربر رو حذف کنی ؟</v-card-title>
-
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn color="blue darken-1" text @click="closeDelete">نه بیخیال</v-btn>
-                            <v-btn color="blue darken-1" text @click="deleteItemConfirm">آره صد در صد</v-btn>
-                            <v-spacer></v-spacer>
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
-
-                <v-icon small class="mr-2" @click="editItem(item)">
-                    mdi-pencil
-                </v-icon>
-                <v-icon small @click="deleteItem(item)">
-                    mdi-delete
-                </v-icon>
-            </template>
-
-        </v-data-table>
-    </v-card>
-
-</v-card>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="close">
+                            Cancel
+                        </v-btn>
+                        <v-btn color="blue darken-1" text @click="save">
+                            Save
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+                <v-card>
+                    <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                        <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteItem(item)">
+            mdi-delete
+        </v-icon>
+    </template>
+    <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize">
+            Reset
+        </v-btn>
+    </template>
+</v-data-table>
 </template>
 
 <script>
@@ -60,23 +91,22 @@ export default {
                     value: 'slug'
                 },
                 {
-                    text: 'ایمیل',
-                    value: 'fat'
-                },
-                {
-                    text: 'شماره تماس',
-                    value: 'carbs'
-                },
-                {
-                    text: 'پست ها',
-                    value: 'protein'
-                },
-                {
                     text: 'عملیات',
                     value: 'actions'
                 },
             ],
             usersInfoList: [],
+            editedIndex: -1,
+            editedItem: {
+                name: '',
+                slug: 0,
+
+            },
+            defaultItem: {
+                name: '',
+                slug: 0,
+
+            },
         }
     },
     methods: {
@@ -101,6 +131,12 @@ export default {
 
         },
 
+        editItem(item) {
+            this.editedIndex = this.usersInfoList.indexOf(item)
+            this.editedItem = Object.assign({}, item)
+            this.dialog = true
+        },
+
         deleteItem(item) {
             console.log(item.id);
             this.editedIndex = this.usersInfoList.indexOf(item)
@@ -123,6 +159,13 @@ export default {
             this.usersInfoList.splice(this.editedIndex, 1)
             this.closeDelete()
         },
+        close() {
+            this.dialog = false
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            })
+        },
 
         closeDelete() {
             this.dialogDelete = false
@@ -131,7 +174,20 @@ export default {
                 this.editedIndex = -1
             })
         },
+        save() {
+            if (this.editedIndex > -1) {
+                Object.assign(this.desserts[this.editedIndex], this.editedItem)
+            } else {
+                this.desserts.push(this.editedItem)
+            }
+            this.close()
+        },
 
+    },
+    computed: {
+        formTitle() {
+            return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+        },
     },
     watch: {
         dialog(val) {
@@ -143,6 +199,7 @@ export default {
     },
     created() {
         this.getUsers();
+        //this.initialize()
     },
 }
 </script>
